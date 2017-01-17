6b42310aa228967bedfde3346075a0c4ecb73106
kicklive <kicklive@verizon.net>
2017-01-17 10:33:05 -0500

    update test
    testing proxy

--- server.js
+++ server.js
@@ -38,12 +38,14 @@ db.on('error', console.error.bind(console,'connection error....'));
 db.once('open',function(){
     console.log('multivision db opened');
 });
-var messageSchema=mongoose.Schema({message:String});
-var Message=mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err,messageDoc){
-    mongoMessage=messageDoc.message;
-})
+
+
+// var messageSchema=mongoose.Schema({message:String});
+// var Message=mongoose.model('Message', messageSchema);
+// var mongoMessage;
+// Message.findOne().exec(function(err,messageDoc){
+//     mongoMessage=messageDoc.message;
+// })
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -54,7 +56,7 @@ app.get('/partials/:partialPath',function(req,res){
 })
 app.get('*',function(req,res){
     res.render('index',{
-        mongoMessage:mongoMessage
+   //     mongoMessage:mongoMessage
     });
 
 });
